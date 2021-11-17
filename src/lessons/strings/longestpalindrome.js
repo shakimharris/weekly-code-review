5e23f657102b8de74a0526647febeb6de0f89c57
Shakim Harris <amenraprod@gmail.com>
2021-11-17 03:52:34 -0500

    mirror solution

---
+++ src/lessons/strings/longestpalindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(s) {
+  s.toLowerCase(/[\W_]/g, "");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
